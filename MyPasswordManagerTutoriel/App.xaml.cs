6c4b64370ca56d0b96a467761cae7c3ca9d65765
Sylvain <90060355+Sylvain-Mani@users.noreply.github.com>
2023-04-26 16:22:49 +0200

    Initialialisation du tutoriel

---
+++ MyPasswordManagerTutoriel/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyPasswordManagerTutoriel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MyPasswordManagerTutoriel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyPasswordManagerTutoriel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
